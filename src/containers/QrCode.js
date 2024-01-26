eb8ae3bbf1aac1bf1a4aec10da2a77303cf2707b
YuraKolodiy13 <timber2017@gmail.com>
2024-01-26 20:03:28 +0100

    change qrcode logo

--- src/containers/QrCode.js
+++ src/containers/QrCode.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { QRCode } from 'react-qrcode-logo';
+import clockImg from '../assets/img/clock.png';
 
 const QrCode = () => {
 
@@ -8,7 +9,7 @@ const QrCode = () => {
       <QRCode
         size={516}
         value={window.location.href.replace('qrcode', 'game')}
-        logoImage='https://picsum.photos/id/67/300/300'
+        logoImage={clockImg}
       />
     </div>
   );
